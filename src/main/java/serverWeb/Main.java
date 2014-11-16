bdc72128025f629bbf6d9b17d13fa0b9290ea59d
Kenneth Martinez <kenne-3@hotmail.com>
2014-11-16 14:39:16 -0600

    new helloWorld

--- src/main/java/serverWeb/Main.java
+++ src/main/java/serverWeb/Main.java
@@ -4,12 +4,7 @@ import logicaServer.*;
 public class Main {
 
   public static void main(String[] args){
-    System.out.println("hola");
     SocialNetworkServer noob = new SocialNetworkServer();
-    try{
-      noob.startWebServer();
-    }catch(Exception e){
-
-    }
+    noob.startServer();
   }
 }
--- src/main/java/serverWeb/logicaServer/SocialNetworkServer.java
+++ src/main/java/serverWeb/logicaServer/SocialNetworkServer.java
@@ -1,72 +1,37 @@
 package logicaServer;
 
-import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+
+import java.io.IOException;
+
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.*;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.sql.*;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
 
-public class SocialNetworkServer extends HttpServlet{
+public class SocialNetworkServer extends AbstractHandler{
 
+  public void startServer(){
+    try{
+      Server server = new Server(8080);
+      server.setHandler(new SocialNetworkServer());
 
-  public void startWebServer() throws Exception{
-    Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-    context.setContextPath("/");
-    server.setHandler(context);
-    context.addServlet(new ServletHolder(new SocialNetworkServer()),"/*");
-    server.start();
-    server.join();
-  }
-
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-
-    if (req.getRequestURI().endsWith("/db")) {
-      showDatabase(req,resp);
-    } else {
-      showHome(req,resp);
+      server.start();
+      server.join();
+    }catch(Exception e){
+      System.out.println("Running server Failed");
     }
   }
 
-  private void showHome(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    resp.getWriter().print("Hello from Java!");
-  }
+  public void handle(String target,Request baseRequest,
+    HttpServletRequest request, HttpServletResponse response)
 
-  private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    try {
-      Connection connection = getConnection();
-
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      String out = "Hello!\n";
-      while (rs.next()) {
-          out += "Read from DB: " + rs.getTimestamp("tick") + "\n";
-      }
-
-      resp.getWriter().print(out);
-    } catch (Exception e) {
-      resp.getWriter().print("There was an error: " + e.getMessage());
+        throws IOException, ServletException
+    {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello World</h1>");
     }
-  }
-
-  private Connection getConnection() throws URISyntaxException, SQLException {
-    URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-    String username = dbUri.getUserInfo().split(":")[0];
-    String password = dbUri.getUserInfo().split(":")[1];
-    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
-
-    return DriverManager.getConnection(dbUrl, username, password);
-  }
-
 }
